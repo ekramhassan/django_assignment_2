f482fe1c858bb8f9254ebfd62dcbc7ee4c8b0714
ekramhassan <ekramhassan687@gmail.com>
2024-05-20 18:11:53 +0600

    frist commit

---
+++ todo/task/models.py
@@ -0,0 +1,10 @@
+
+
+# Create your models here.
+from django.db import models
+
+class TaskModel(models.Model):
+   
+    taskTitle = models.CharField(max_length=200)
+    taskDescription = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
